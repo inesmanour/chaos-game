5282cf44bcb1e75b86141f372cdfc7a4e51333f5
assadiab <147226737+assadiab@users.noreply.github.com>
2024-04-17 12:17:35 +0200

    mise en commentaire de la commande pour le debug de la matrice

--- chaos_game.py
+++ chaos_game.py
@@ -92,9 +92,10 @@ def generate_chaos_game_representation(seq: str, size: int) -> np.ndarray:
         # Attribution du nucléotide à la section correspondante
         chaos_game_representation[y, x] += 1  # Incrémentation du compteur du nucléotide
 
-    # Affichage de la matrice du Chaos Game (pour le débogage)
+    """# Affichage de la matrice du Chaos Game (pour le débogage)
     print("Matrice du Chaos Game :")
     print(chaos_game_representation)
+    """
 
     # Affichage de la matrice avec Matplotlib
     plt.imshow(chaos_game_representation, cmap='hot', origin='upper')
